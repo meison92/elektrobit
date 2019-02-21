d3a5aabdda765fae93a110f859b772746cceae13
suyiya <542258512@qq.com>
2019-02-21 08:29:26 +0800

    update

--- pages/event/event.js
+++ pages/event/event.js
@@ -1,4 +1,5 @@
 // pages/event/event.js
+const request = require('../../wxApi/request')
 Page({
 
   /**
@@ -28,7 +29,8 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log(request)
+    request('/json/events')
   },
 
   /**
---
+++ wxApi/request.js
@@ -0,0 +1,48 @@
+const API_BASE_URL = 'https://eb.d.dminorstudio.com'
+const request = (url, data,method) => {
+  let _url = API_BASE_URL + url
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: _url,
+      method: method || 'post',
+      data: data,
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      success(request) {
+        resolve(request.data)
+      },
+      fail(error) {
+        reject(error)
+      },
+      complete(aaa) {
+        // 加载完成
+      }
+    })
+  })
+}
+
+/**
+ * 小程序的promise没有finally方法，自己扩展下
+ */
+Promise.prototype.finally = function (callback) {
+  var Promise = this.constructor;
+  return this.then(
+    function (value) {
+      Promise.resolve(callback()).then(
+        function () {
+          return value;
+        }
+      );
+    },
+    function (reason) {
+      Promise.resolve(callback()).then(
+        function () {
+          throw reason;
+        }
+      );
+    }
+  );
+}
+
+module.exports = request
