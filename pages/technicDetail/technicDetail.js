bf34bc0efabc27997c8be6a4ffc12d9c5b0fee67
yiya.su <542258512@qq.com>
2020-04-29 10:06:14 +0800

    update

--- pages/technicDetail/technicDetail.js
+++ pages/technicDetail/technicDetail.js
@@ -1,5 +1,5 @@
 // pages/technicDetail/technicDetail.js
-import { getTechnologyDetail, submitComment } from '../../wxApi/request'
+import { getTechnologyDetail, submitComment, sendEmail } from '../../wxApi/request'
 const app = getApp();
 Page({
 
@@ -106,6 +106,70 @@ Page({
   bindChange: function (event) {
     this.setData({
       comment: event.detail.value
+    })
+  },
+
+  preview: function () {
+    let url = this.data.data.file_downloads[0].url;
+    wx.navigateTo({
+      url: `/pages/webview/webview?link=${url}`
+    })
+  },
+  sendToEmail: function () {
+    let id = this.options.id;
+    if (!app.globalData.userInfo) {
+      wx.reLaunch({
+        url: '/pages/login/login'
+      })
+      return;
+    }
+    if (!app.globalData.userInfo.email) {
+      wx.switchTab({
+        url: '/pages/mine/mine',
+        success: (result) => {
+          wx.showToast({
+            title: '请补充邮箱信息',
+            icon: 'none',
+            image: '',
+            duration: 1500,
+            mask: false,
+            success: (result) => {
+
+            },
+            fail: () => { },
+            complete: () => { }
+          });
+        },
+        fail: () => { },
+        complete: () => { }
+      });
+      return;
+    }
+    const params = {
+      id: id,
+      data: {
+        email: app.globalData.userInfo.email
+      }
+    }
+    wx.showToast({
+      title: '发送中',
+      icon: 'none',
+      image: '',
+      duration: 10000,
+      mask: false,
+      success: (result) => {
+
+      },
+      fail: () => { },
+      complete: () => { }
+    });
+    sendEmail(params).then(res => {
+      console.log(res)
+      wx.showToast({
+        title: '邮件发送成功',
+        icon: 'success',
+        duration: 2000
+      })
     })
   }
 })
