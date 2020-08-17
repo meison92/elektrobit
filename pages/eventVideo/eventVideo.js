99c3350b5af2e5823d02f1cc447bb74fcf6f4ac1
yiya.su <542258512@qq.com>
2020-08-17 21:58:40 +0800

    update

--- pages/eventVideo/eventVideo.js
+++ pages/eventVideo/eventVideo.js
@@ -1,6 +1,7 @@
 // pages/eventDetail/eventDetail.js
 var WxParse = require('../../wxParse/wxParse.js');
-import { getEventDetail } from '../../wxApi/request'
+const app = getApp();
+import { getEventDetail, sendEmail } from '../../wxApi/request'
 Page({
 
   /**
@@ -103,4 +104,62 @@ Page({
       })
     })
   },
+
+  sendToEmail: function () {
+    let id = this.options.id;
+    if (!app.globalData.userInfo) {
+      wx.navigateTo({
+        url: '/pages/login/login'
+      })
+      return;
+    }
+    if (!app.globalData.userInfo.email) {
+      wx.switchTab({
+        url: '/pages/mine/mine',
+        success: (result) => {
+          wx.showToast({
+            title: '请补充邮箱信息',
+            icon: 'none',
+            image: '',
+            duration: 1500,
+            mask: false,
+            success: (result) => {
+
+            },
+            fail: () => { },
+            complete: () => { }
+          });
+        },
+        fail: () => { },
+        complete: () => { }
+      });
+      return;
+    }
+    const params = {
+      id: id,
+      data: {
+        email: app.globalData.userInfo.email
+      }
+    }
+    wx.showToast({
+      title: '发送中',
+      icon: 'none',
+      image: '',
+      duration: 10000,
+      mask: false,
+      success: (result) => {
+
+      },
+      fail: () => { },
+      complete: () => { }
+    });
+    sendEmail(params).then(res => {
+      console.log(res)
+      wx.showToast({
+        title: '邮件发送成功',
+        icon: 'success',
+        duration: 2000
+      })
+    })
+  }
 })
